b9317027c4d7627d5684113eca2fef865df92ea0
russellsutt <russell.sutter@gmail.com>
2020-10-16 09:55:19 -0400

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/tempIndex.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('that dude left')
+  })
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
